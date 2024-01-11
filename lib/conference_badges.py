403a0205f271a3a721a94126808048119ef63d9c
hadil <hihijazi@gmail.com>
2024-01-10 21:02:34 -0500

    save changes

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    # list_names = []
+    # for name in names:
+    #     list_names.append(badge_maker(name))
+    # return list_names
+    names= [badge_maker(name) for name in names]
+    return names
 
 def assign_rooms(names):
-    return None
+    list = []
+    for i in range(len(names)):
+        list.append(f'Hello, {names[i]}! You\'ll be assigned to room {i + 1}!')
+    return list
 
 def printer(names):
-    return None
+    list_badge= batch_badge_creator(names)
+    for name in list_badge:
+        print (name)
+    list_rooms = assign_rooms(names)  
+    for name in list_rooms:
+        print(name)
